f04ccd85736e5bc364ed29fce721ef944d60af06
nanamuramarina <nanamuramarina@gmail.com>
2024-01-02 13:17:55 +0000

    サーブレットの基本

---
+++ FirstServlet.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/first")
+public class FirstServlet extends HttpServlet {
+  public void doGet(HttpServletRequest req, HttpServletResponse res)
+    throws IOException, ServletException {
+    res.setContentType("text/html;charset=utf-8");
+    PrintWriter out = res.getWriter();
+    out.println("<html>");
+    out.println("<head>");
+    out.println("<title>はじめてのサーブレット</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>サーブレットを始めよう！</h1>");
+    out.println("</body>");
+    out.println("</html>");
+  }
+}
