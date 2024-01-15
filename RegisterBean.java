03d240b0db560b8387a7b7f8825437ff593d392f
nanamuramarina <nanamuramarina@gmail.com>
2024-01-15 13:30:30 +0000

    RegisterBean

---
+++ RegisterBean.java
@@ -0,0 +1,37 @@
+package bean;
+
+import　java.io.Serializable;
+
+public class RegisterBean implements Serializable {
+  private String name;
+  private String age;
+  private String[] langs;
+
+  public RegisterBean() {
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+  public void setAge(String age){
+    this.age = age;
+  }
+  public void setLangs(String[] langs){
+    this.langs = langs;
+  }
+  
+  public String getName(){
+    return name;
+  }
+  public String getAge(){
+    return age;
+  }
+  public String getLangs(){
+    return langs;
+  }
+  
+  public String getJpnAge(){
+    String strLangs = "";
+    for (int i = 0;)
+  }
+}
